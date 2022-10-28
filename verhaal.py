085e9858fd39edab155f38452d6c7968577c48bd
IsmailToy <114576312+IsmailToy@users.noreply.github.com>
2022-10-28 15:21:17 +0200

    Add files via upload

---
+++ verhaal.py
@@ -0,0 +1,145 @@
+rungame = 0
+Verhaallijn2 = 0
+Verhaallijn3 = 0
+Verhaallijn4 = 0
+Verhaallijn5 = 0
+Verhaallijn6 = 0
+Verhaallijn7 = 0
+Verhaallijn8 = 0
+Verhaallijn9 = 0
+Verhaallijn10 = 0
+Verhaallijn11 = 0
+Verhaallijn12 = 0
+Verhaallijn13 = 0
+Verhaallijn14 = 0
+Verhaallijn15 = 0
+Verhaallijn16 = 0
+Verhaallijn17 = 0
+Verhaallijn18 = 0
+Verhaallijn19 = 0
+Verhaallijn20 = 0
+Verhaallijn21 = 0
+
+print("Verhaallijn 1" )
+print("Verhaallijn1Vraag" )
+choice = input()
+if choice == 'Choice1':
+    print("Verhaallijn 2" )
+    Verhaallijn2 += 1
+elif choice == 'Choice2':
+    print("Verhaallijn 3")
+    Verhaallijn3 += 1
+else:
+    print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn2Vraag" )
+    choice = input()
+    if choice == 'Choice3':
+        print("Verhaallijn 4" )
+        Verhaallijn4 += 1
+    elif choice == 'Choice4':
+        print("Verhaallijn 5")
+        Verhaallijn5 += 1
+    elif choice == 'Choice5':
+        print("Verhaallijn 6")
+        Verhaallijn6 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn3Vraag" )
+    choice = input()
+    if choice == 'Choice6':
+        print("Verhaallijn 7" )
+        Verhaallijn7 += 1
+    elif choice == 'Choice7':
+        print("Verhaallijn 8")
+        Verhaallijn8 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn4Vraag" )
+    choice = input()
+    if choice == 'Choice8':
+        print("Verhaallijn 9" )
+        Verhaallijn9 += 1
+    elif choice == 'Choice9':
+        print("Verhaallijn 10")
+        Verhaallijn10 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn5Vraag" )
+    choice = input()
+    if choice == 'Choice10':
+        print("Verhaallijn 11" )
+        Verhaallijn11 += 1
+    elif choice == 'Choice12':
+        print("Verhaallijn 12")
+        Verhaallijn12 += 1
+    elif choice == 'Choice13':
+        print("Verhaallijn 13")
+        Verhaallijn13 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn6Vraag" )
+    choice = input()
+    if choice == 'Choice14':
+        print("Verhaallijn 14" )
+        Verhaallijn14 += 1
+    elif choice == 'Choice15':
+        print("Verhaallijn 15")
+        Verhaallijn15 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn7Vraag" )
+    choice = input()
+    if choice == 'Choice16':
+        print("Verhaallijn 16" )
+        Verhaallijn16 += 1
+    elif choice == 'Choice17':
+        print("Verhaallijn 17")
+        Verhaallijn17 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn8Vraag" )
+    choice = input()
+    if choice == 'Choice18':
+        print("Verhaallijn 18" )
+        Verhaallijn18 += 1
+    elif choice == 'Choice19':
+        print("Verhaallijn 19")
+        Verhaallijn19 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+if Verhaallijn2 == 1:
+    print("Verhaallijn9 Vraag" )
+    choice = input()
+    if choice == 'Choice20':
+        print("Verhaallijn 20" )
+        Verhaallijn20 += 1
+    elif choice == 'Choice21':
+        print("Verhaallijn 21")
+        Verhaallijn21 += 1
+    else:
+        print(choice, " wasn't a valid choice")
+
+restart = input ()
+print ("Do you wish to restart..? ")
+if restart == 'RESTART':
+          rungame = 0
+
+else: 
+    if restart == 'RESTART': 
+        print()
+    exit ()   
